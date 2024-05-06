8ea5ec7a25177b060a49d3430ddb3a0e5bc5e824
Clay Ewing <c.ewing@miami.edu>
2024-05-06 12:41:27 -0500

    new node templates for image reference
    Add Image to Canvas adds an image into a conversation
    Remove Image From Canvas removes an image from a conversation
    Object Close Up activates a separate canvas for a separate mechanic, hiding the dialogue system completely.

--- Assets/Scripts/ClickOutsideToClose.cs
+++ Assets/Scripts/ClickOutsideToClose.cs
@@ -3,14 +3,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using VNEngine;
 
 public class ClickOutsideToClose : MonoBehaviour
 {
+
+    public bool ContinueConversationOnClick = false;
+
     void Update()
     {
         if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2))
         {
             this.gameObject.SetActive(ClickingSelfOrChild());
+            if(ContinueConversationOnClick)
+            {
+                VNSceneManager.Waiting_till_true = true;
+            }
         }
     }
     private bool ClickingSelfOrChild()

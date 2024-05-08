aabf127cf6b4f2e38e69c0d1a131fd4734c808ce
Clay Ewing <c.ewing@miami.edu>
2024-05-08 13:51:53 -0500

    polish thinking, begin asset collection for map

--- Assets/VN Engine/Scripts/Nodes/NodeThink.cs
+++ Assets/VN Engine/Scripts/Nodes/NodeThink.cs
@@ -5,127 +5,71 @@ using TMPro;
 
 namespace VNEngine
 {
-    // Not used in real code. Merely a template to copy and paste from when creating new nodes.
     public class NodeThink : Node
     {
-        public GameObject thinkingCanvas;
-        public TextMeshProUGUI display;
-
-        private bool thinking = true;
-
-        public string[] thoughts;
+        public Thought thought;
+        public Sprite characterImage;
+        public TranslatedText[] thoughts;
         public float timeBetweenThoughts = 5.0f;
 
+        private bool thinking = true;
         private float nextThoughtTime;
         private int thoughtIndex = 0;
-        
-
-        private Vector3[] desiredPosition;
 
         // Called initially when the node is run, put most of your logic here
         public override void Run_Node()
         {
-            thinkingCanvas.SetActive(true);
+            thought.gameObject.SetActive(true);
+            thought.SetCharacter(characterImage);
+            thought.SetThought(thoughts[thoughtIndex]);
+            thought.ResetColors();
             StartCoroutine(Thinking_Coroutine());
-            
-            //            VNSceneManager.scene_manager.Show_UI(false);  // Ensure dialogue panel is visible
-
-            //          running = true;
-            //            UIManager.ui_manager.GetComponentInParent<Think>().show();
-
-            // if there's no need to  wait for other operations/coroutines, call finish node at the end of this method
-            Finish_Node();
         }
-
-
-        // What happens when the user clicks on the dialogue text or presses spacebar? Either nothing should happen, or you call Finish_Node to move onto the next node
-        public override void Button_Pressed()
-        {
-            //Finish_Node();
-        }
-
 
         // Fades the image from opaque to transparent
         IEnumerator Thinking_Coroutine()
         {
-
             while (thinking)
             {
-                Debug.Log("NEXT THOUGHT TIME: " + nextThoughtTime);
-
                 if (nextThoughtTime <= Time.time)
                 {
-                    Debug.Log("Setting next thought");
-
                     // Check if there are more thoughts to display
-                    if (thoughtIndex < thoughts.Length)
+                    if (thoughtIndex < thoughts.Length - 1)
                     {
-                        display.text = thoughts[thoughtIndex];
-                        thoughtIndex++;
+                        if(thoughtIndex != 0)
+                        {
+                            Debug.Log("Fading Out Previous Thought");
+                            // Fade out previous thought
+                            thought.FadeOutText();
+                        }
+                        yield return new WaitForSeconds(timeBetweenThoughts);
                     }
 
-                    // Set the time for the next thought
-                    nextThoughtTime = Time.time + timeBetweenThoughts;
-                    Debug.Log("NEXT THOUGHT TIME: " + nextThoughtTime);
+                    thoughtIndex++;
+                    
+                    if(thoughtIndex >= thoughts.Length)
+                    {
+                        thinking = false;
+                    }
+                    else
+                    {
+                        thought.SetThought(thoughts[thoughtIndex]);
+                        // Set the time for the next thought
+                        nextThoughtTime = Time.time + timeBetweenThoughts;
+                    }
                 }
 
-                // Check if all thoughts are displayed
-                if (thoughtIndex >= thoughts.Length)
-                {
-                    thinking = false;
-                    Debug.Log("All thoughts displayed.");
-                }
-                if(thinking)
-                {
-                    // Yield for the next frame
-                    yield return null;
-
-                }
-                else
-                {
-                    Finish_Node();
-                }
+                yield return null;
             }
 
-            /*
-            Actor[] actors = ActorManager.actors_on_scene.ToArray();
-            desiredPosition = new Vector3[actors.Length];
-
-            for (int i = 0; i < actors.Length; i++)
-            {
-                desiredPosition[i] = actors[i].desired_position;
-                if (thinking)
-                {
-//                    actors[i].Place_At_Position()
-                    desiredPosition[i].y = -Screen.height * 2f;
-
-                }
-                else
-                {
-                    desiredPosition[i].y = +Screen.height * 2f;
-
-                }
-                actors[i].desired_position += desiredPosition[i];
-                Debug.Log("ACTOR POSITION: " + actors[i].desired_position);
-
-            }
-            */
+            // All thoughts displayed, finish node
+            Finish_Node();
         }
 
-
-
-
-
-        // Do any necessary cleanup here, like stopping coroutines that could still be running and interfere with future nodes
+        // Do any necessary cleanup here
         public override void Finish_Node()
         {
-            if(!thinking)
-            {
-                StopAllCoroutines();
-                thinkingCanvas.SetActive(false);
-
-            }
-
+            thought.gameObject.SetActive(false);
             base.Finish_Node();
         }
     }

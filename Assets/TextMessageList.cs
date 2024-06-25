08918c8eeee0c7641d4e16eaf407c364629edb61
Clay Ewing <c.ewing@miami.edu>
2024-06-25 12:47:30 -0500

    phone updates
    using placeholder UI

--- Assets/TextMessageList.cs
+++ Assets/TextMessageList.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SceneManagement;
+using System;
+using System.Text.RegularExpressions;
 
 [System.Serializable]
 public struct ProfilePicture
@@ -15,19 +17,28 @@ public struct ProfilePicture
 public class TextMessageList : MonoBehaviour
 {
     public GameObject listItemTemplate;
-    public ProfilePicture[] profiles;    
-
+    public GameObject messageTemplate;
+    public GameObject inbox;
+    public TextMeshProUGUI inboxHeader;
+    public Image inboxProfile;
+    public Button likeMessageButton;
+    
+    public ProfilePicture[] profiles;
+    private List<TextMessage> messages;
     // Start is called before the first frame update
     void Start()
     {
-        List<TextMessage> messages = PlayerPrefsExtra.GetList<TextMessage>("messages", new List<TextMessage>());
+        messages = PlayerPrefsExtra.GetList<TextMessage>("messages", new List<TextMessage>());
         for(int i = 0; i < messages.Count; i++)
         {
             GameObject go = Instantiate(listItemTemplate, transform);
-            go.GetComponent<ViewTextMessage>().message.text = messages[i].message;
+//            go.GetComponent<ViewTextMessage>().message.text = messages[i].message;
             Button button = go.GetComponent<Button>();
-            string loc = messages[i].location;
-            button.onClick.AddListener(() => GoToLocation(loc));
+//            string loc = messages[i].location;
+            Character from = messages[i].from;
+            //change to view message
+            //            button.onClick.AddListener(() => GoToLocation(loc));
+            button.onClick.AddListener(() => LoadThread(from, messages));
             go.GetComponent<ViewTextMessage>().from.text = messages[i].from.ToString();
             for(int j = 0; j < profiles.Length; j++)
             {
@@ -39,15 +50,65 @@ public class TextMessageList : MonoBehaviour
         }
     }
 
+    private void ClearMessages()
+    {
+        ViewTextMessage[] list = inbox.GetComponentsInChildren<ViewTextMessage>();
+        Debug.Log("FOUND " + list.Length + "messages");
+        for (int i = 0; i < list.Length; i++)
+        {
+            Destroy(list[i].gameObject);
+        }
+    }
+
+    private void LoadThread(Character from, List<TextMessage> messages)
+    {
+        Debug.Log("LOADING THREAD FOR " + from.ToString());
+        ClearMessages();
+        inboxHeader.text = from.ToString();
+        inbox.transform.parent.parent.parent.gameObject.SetActive(true);
+        for (int i = 0; i < messages.Count; i++)
+        {
+            if(messages[i].from == from)
+            {
+                for (int j = 0; j < profiles.Length; j++)
+                {
+                    if (profiles[j].character == messages[i].from)
+                    {
+                        inboxProfile.sprite = profiles[j].picture;
+                    }
+                }
+
+                //CREATE (MULTIPLE) MESSAGE
+                string[] sentences = Regex.Split(messages[i].message, @"(?<=[\.!\?])\s+");
+                Debug.Log("FOUND " + sentences.Length + " SENTENCES.");
+                for(int j = 0; j < sentences.Length; j++)
+                {
+                    GameObject go = Instantiate(messageTemplate, inbox.transform);
+                    go.GetComponent<ViewTextMessage>().message.text = sentences[j];
+                }
+                //change to view message
+                string loc = messages[i].location;
+                likeMessageButton.onClick.AddListener(() => GoToLocation(loc));
+
+//                go.GetComponent<ViewTextMessage>().from.text = messages[i].from.ToString();
+/*
+                for (int j = 0; j < profiles.Length; j++)
+                {
+                    if (profiles[j].character == messages[i].from)
+                    {
+                        go.GetComponent<ViewTextMessage>().profile.sprite = profiles[j].picture;
+                    }
+                }
+*/
+            }
+        }
+        transform.parent.parent.parent.gameObject.SetActive(false);
+    }
+
     private void GoToLocation(string location)
     {
         Debug.Log("Going to " + location);
         SceneManager.LoadScene(location);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/TextThread.cs
+++ Assets/TextThread.cs
@@ -1,16 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
 
-public class ViewTextMessage : MonoBehaviour
+public class TextThread : MonoBehaviour
 {
-    public TextMeshProUGUI from;
-    public TextMeshProUGUI message;
-    public TextMessage txt;
-    public Image profile;
-
     // Start is called before the first frame update
     void Start()
     {
